ca2afb2d7a2d37831b538f1ea3f27aa8678ca7a1
rch79 <rhorta@gmail.com>
2022-02-13 00:09:46 +0000

    Set up admin page

--- counselling_appts/admin.py
+++ counselling_appts/admin.py
@@ -3,5 +3,32 @@ from .models import Appointment, Message
 
 # Register your models here.
 
-admin.site.register(Appointment)
-admin.site.register(Message)
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('date', 'status', 'client', 'comment')
+    list_filter = ('date', 'client')
+    actions = ['confirm_appointment', 'cancel_appointment', 'set_to_pending_confirmation']
+
+    def confirm_appointment(self, request, queryset):
+        queryset.update(status = 1)
+
+    def cancel_appointment(self, request, queryset):
+        queryset.update(status = 2)
+
+    def set_to_pending_confirmation(self, request, queryset):
+        queryset.update(status = 0)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('date', 'client', 'status')
+    list_filter = ('client', 'status')
+    actions = ['mark_as_read', 'mark_as_unread', 'mark_as_replied']
+
+    def mark_as_unread(self, request, queryset):
+        queryset.update(status = 0)
+
+    def mark_as_read(self, request, queryset):
+        queryset.update(status = 1)
+
+    def mark_as_replied(self, request, queryset):
+        queryset.update(status = 2)
