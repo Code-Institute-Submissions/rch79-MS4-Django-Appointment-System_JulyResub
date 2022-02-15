e8b1ef5e127ff967192de1ce62a070d48e716d96
rch79 <rhorta@gmail.com>
2022-02-15 00:16:09 +0000

    Add placeholder home page

--- counselling_appts/urls.py
+++ counselling_appts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
     path('appointments', views.AppointmentListView.as_view(), name='appointments')
 ]
--- counselling_appts/views.py
+++ counselling_appts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Appointment
 
+class HomePageView(generic.TemplateView):
+    template_name = 'index.html'
+
 
 class AppointmentListView(generic.ListView):
     model = Appointment
