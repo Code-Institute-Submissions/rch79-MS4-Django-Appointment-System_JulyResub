52b110e6c29df09bc1bdadac6191d609358e4864
rch79 <rhorta@gmail.com>
2022-05-20 13:59:40 +0000

    Rewrite appointment list view and template

--- counselling_appts/urls.py
+++ counselling_appts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('appointments/', views.AppointmentListView.as_view(), name='appointments'),
+    path('appointments/', views.list_appointments, name='appointments'),
     path('contact_us/', views.MessageView.as_view(), name='contact_us'),
     path('booking/', views.AppointmentBookingView.as_view(), name='appointment_booking'),
 ]
--- counselling_appts/views.py
+++ counselling_appts/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Appointment
+
+#redirects non-authenticated users to login page
+from django.contrib.auth.decorators import login_required
+
+from .models import Appointment, User
 
 class HomePageView(generic.TemplateView):
     template_name = 'index.html'
 
 
-class AppointmentListView(generic.ListView):
-    model = Appointment
-    queryset = Appointment.objects.all()
-    template_name = 'appointments.html'
+# class AppointmentListView(generic.ListView):
+#     model = Appointment
+#     queryset = Appointment.objects.all()
+#     template_name = 'appointments.html'
 
 
 class MessageView(generic.TemplateView):
@@ -18,3 +22,24 @@ class MessageView(generic.TemplateView):
 
 class AppointmentBookingView(generic.TemplateView):
     template_name = 'counselling_appts/appointment_booking.html'
+
+@login_required
+def list_appointments(request):
+    """
+    Displays scheduled appointments for logged in clients
+    Superusers ill be shown all scheduled appointments
+    for all clients
+    """
+
+    # client = get_object_or_404(request.user)
+
+    if request.user.is_superuser:
+        appointments = Appointment.objects.all()
+    else:
+        appointments = Appointment.objects.filter(client__exact=request.user)
+
+    context = {
+        'appointments': appointments,
+    }
+
+    return render(request, 'counselling_appts/appointments.html', context)
